ab2e42b24e92f7d9b3ace03a762428e5540f4c6d
dfuentes22 <plainpython@gmail.com>
2021-10-08 18:10:23 -0400

    reset

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,9 @@ import React from "react"
 
 export default function Home() {
   return (
-    <div style={{ color: "tomato", backgroundColor: "blue" }}>
+    <div style={{ color: "tomato" }}>
       <h1>Hello world</h1>
       <p>This is a paragraph</p>
-      <img
-        src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg"
-        alt="dog"
-      />
     </div>
   )
 }
