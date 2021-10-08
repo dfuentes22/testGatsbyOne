352db14f4d8d8d64885a48cda7041fe72c9f5ddf
dfuentes22 <plainpython@gmail.com>
2021-10-08 18:17:33 -0400

    add new pages and link to them

--- src/pages/dir1/page-3.js
+++ src/pages/dir1/page-3.js
@@ -4,10 +4,8 @@ import { Link } from "gatsby"
 export default function Home() {
   return (
     <div style={{ color: "tomato" }}>
-      <h1>Hello world</h1>
-      <p>This is a paragraph</p>
-      <Link to="/page-2/">Page 2</Link>
-      <Link to="/dir1/page-3/">Page 3</Link>
+      <h1>Page 3</h1>
+      <Link to="/">Go Home</Link>
     </div>
   )
 }
