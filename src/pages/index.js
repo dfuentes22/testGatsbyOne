179ec28f032992998d684ba9b6ea26849b13ed96
dfuentes22 <plainpython@gmail.com>
2021-10-08 19:36:58 -0400

    created layout compononent

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
 import Counter from "./counter"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <div style={{ color: "tomato" }}>
-      <h1>Hello world</h1>
-      <p>This is a paragraph</p>
-      <Link to="/page-2/">Page 2</Link>
-      <Link to="/dir1/page-3/">Page 3</Link>
-      <Counter color="green" />
+    <div>
+      outside layout
+      <Layout>
+        <h1>Hello world</h1>
+        <p>This is a paragraph</p>
+        <Link to="/page-2/">Page 2</Link>
+        <Link to="/dir1/page-3/">Page 3</Link>
+        <Counter color="green" />
+      </Layout>
+      outside layout
     </div>
   )
 }
