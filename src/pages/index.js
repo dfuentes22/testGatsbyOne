27286b32c244ed0fc3cae3c17b552b2c7a7fe7e3
dfuentes22 <plainpython@gmail.com>
2021-10-08 18:05:06 -0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: "tomato", backgroundColor: "blue" }}>
+      <h1>Hello world</h1>
+      <p>This is a paragraph</p>
+      <img
+        src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg"
+        alt="dog"
+      />
+    </div>
+  )
 }
